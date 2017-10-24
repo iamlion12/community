397723d322754c3b084238a703087152c947b3ae
iamlion12 <ktd.iamlion12@gmail.com>
2017-10-24 13:13:24 +0500

    End

--- community/commapp/urls.py
+++ community/commapp/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'index', views.index, name='index'),
     url(r'^community/(?P<pk>\d+)/$', views.community, name='community'),
     url(r'^post/new/$', views.new_post, name='new_post'),
+    url(r'calendar', views.calendar, name='calendar'),
 ]
--- community/commapp/views.py
+++ community/commapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Coms, Post
 from .forms import PostForm
+from django.shortcuts import redirect
 
 
 def index(request):
@@ -14,5 +15,15 @@ def community(request, pk):
     return render(request, 'community.html', {'com': com})
 
 def new_post(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('community', group=pk)
+    else:
+        form = PostForm()
     return render(request, 'new_post.html', {'form': form})
+
+def calendar(request):
+    return render(request, 'calendar.html')
